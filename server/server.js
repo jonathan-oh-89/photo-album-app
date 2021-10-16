25f15e6a7f9c3e3073fdfd08ec00b398553b3757
Jon Oh <jonathano@zillowgroup.com>
2021-10-16 13:05:54 -0700

    debug api

--- server/server.js
+++ server/server.js
@@ -135,7 +135,7 @@ app.post('/retrieveimages', isAuth, async (req, res, next) => {
 app.get('/retrieveallimages', async (req, res) => {
     console.log("*** RECEIVED RETRIEVEALLIMAGES POST");
     const photoArray = await cloudinaryRetrieveAll()
-    res.status(200).send(photoArray)
+    res.status(200).send({ 'message': 'something wrong', 'data': photoArray })
 })
 
 app.post('/uploadimages', isAuth, async (req, res) => {
